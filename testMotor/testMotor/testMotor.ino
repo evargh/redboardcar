b72b8f7b0d781791f857d97deb632b0486fa3c88
evargh <varghese.evan@gmail.com>
2019-12-02 11:37:15 -0600

    Update testMotor.ino

--- testMotor/testMotor/testMotor.ino
+++ testMotor/testMotor/testMotor.ino
@@ -1,74 +1,46 @@
-#include <RedBot.h>
-
 const int AIN1 = 10;
 const int AIN2 = 11;
-
-const int BIN1 = 6;
-const int BIN2 = 5;
-
 const int PWMA = 13;
+
 
 int motorSpeed = 0;
 
 void setup() {
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(BIN2, OUTPUT);
   pinMode(PWMA, OUTPUT);
 
   Serial.begin(9600);
-
+  Serial.println("Enter motor speed (0-255)...");
 }
 
 void loop() {
- Serial.print("start running");
- spinMotor(2);
- delay(4000);
- spinMotor(1);
- delay(4000);
- spinMotor(0);
- delay(4000);
- spinMotor(-1);
- delay(4000);
- spinMotor(-2);
- delay(4000);
- spinMotor(-3);
- delay(4000);
+  if(Serial.available() > 0){
+    motorSpeed = Serial.parseInt();
+git 
+    Serial.print("Motor Speed: ");
+    Serial.println(motorSpeed);
+  }
+
+  spinMotor(motorSpeed);
 }
 
 void spinMotor(int motorSpeed)
 {
-  if(motorSpeed >= 0)
+  if(motorSpeed > 0)
   {
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
-    digitalWrite(BIN1, HIGH);
-    digitalWrite(BIN2, LOW);
   }
-  if(motorSpeed == -1)
-  {
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, LOW);
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, HIGH);
-  }
-  if(motorSpeed == -2)
+  else if(motorSpeed < 0)
   {
     digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, HIGH);
-    digitalWrite(BIN1, HIGH);
-    digitalWrite(BIN2, LOW);
   }
-  if(motorSpeed == -3)
+  else
   {
     digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, LOW);
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, LOW);
   }
-  if(motorSpeed == 2)
-    analogWrite(PWMA, 80);
-  else
-    analogWrite(PWMA, 64);
+  analogWrite(PWMA, abs(motorSpeed));
 }
