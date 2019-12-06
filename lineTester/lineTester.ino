9ee63d23ed128e865ab01fe051f7cfd85026f485
noraagah <55000129+noraagah@users.noreply.github.com>
2019-12-06 10:24:07 -0600

    Update lineTester.ino

--- lineTester/lineTester.ino
+++ lineTester/lineTester.ino
@@ -3,13 +3,13 @@
 
 //this code tests what the levels should be
 
-RedBotSensor cSen = RedBotSensor(A1);
+RedBotSensor cSen = RedBotSensor(A0);
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print(cSen.read());
+  Serial.println(cSen.read());
   delay(100);
 }
