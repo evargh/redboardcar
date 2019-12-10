bfb832b54ae5e9ecd4679aa649cba080e2156df0
Adrian Hinojosa <adrian010501@gmail.com>
2019-12-10 11:51:58 -0600

    testMotor change

--- testMotor/testMotor/testMotor.ino
+++ testMotor/testMotor/testMotor.ino
@@ -7,38 +7,52 @@ int motorSpeed = 0;
 void setup() {
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWM, OUTPUT);
 
   Serial.begin(9600);
-  Serial.println("Enter motor speed (0-255)...");
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    motorSpeed = Serial.parseInt();
-    Serial.print("Motor Speed: ");
-    Serial.println(motorSpeed);
-  }
-
-  spinMotor(motorSpeed);
+  spinMotor(2);
+  delay(100);
+  spinMotor(1)
+  delay(10000);
 }
 
-void spinMotor(int motorSpeed)
-{
-  if(motorSpeed > 0)
+void spinMotor(int motorSpeed) {
+  Serial.println(motorSpeed);
+  if(motorSpeed == 1)
+    analogWrite(PWM, 75);
+  else
+    analogWrite(PWM, 150);
+  if(motorSpeed >= 0)
   {
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
   }
-  else if(motorSpeed < 0)
+  if(motorSpeed == -1)
   {
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, HIGH);
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
   }
-  else
+  if(motorSpeed == -2)
   {
     digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
   }
-  analogWrite(PWMA, abs(motorSpeed));
+  if(motorSpeed == -3)
+  {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
+  }
 }
